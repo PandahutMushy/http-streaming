3ef64db2d9fc33885b5b91723aee170c8072c061
ferrazf <ferrazf9@gmail.com>
2019-04-06 23:27:22 +0000

    Parsing streamed data

--- https_example.js
+++ https_example.js
@@ -5,8 +5,13 @@ var options = {
 };
 
 // called by https when the request is made.
-var callback = function() {
-  console.log("In response handler callback - Asynchronous call");
+var callback = function(response) {
+  console.log("In response handler callback!");
+
+  response.on("data", function(chunk) {
+    console.log("[-- CHUNK OF LENGTH " + chunk.length + " --]");
+    console.log(chunk.toString());
+  });
 };
 
 console.log("I'm about to make the request!");
